7d5b8fc3151d5af89e2b210a8ebdc08b9c4fec98
nehalchoraria <nehalchoraria09@gmail.com>
2018-02-28 00:44:43 +0530

    .

---
+++ Sort-Algorithms/insertionsort.py
@@ -0,0 +1,9 @@
+def insertionsort(arr):
+	for rightindex in range(len(arr)):     #For every element in array
+		checkpoint = rightindex       		# keep comparing with previous elements until element behind is 
+		while checkpoint>0 and arr[checkpoint] < arr[checkpoint - 1]:	# smaller and keep swapping
+			(arr[checkpoint],arr[checkpoint-1]) = (arr[checkpoint-1],arr[checkpoint])
+			checkpoint = checkpoint - 1
+	print(arr)
+
+insertionsort(list(range(500,0,-1)))
