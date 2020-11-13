240d8ca33d823e8c75d4980dccaa7cbc62cc17c2
unknown <nehalchoraria09@gmail.com>
2020-11-13 10:10:50 +0530

    completed heap sort

--- Heap/heapify.py
+++ Heap/heapify.py
@@ -1,4 +1,5 @@
 
+# Assuming Left heap and right heap being in order build max heap on index
 def maxHeapify(array,index):
     leftIndex = 2*index
     rightIndex = 2*index + 1
@@ -12,19 +13,31 @@ def maxHeapify(array,index):
         array[largest],array[index] = array[index],array[largest]
         maxHeapify(array,largest)
 
+# Starting from leaf nodes to root node start heapify iteratively
 def buildHeap(array):   
     for i in range(len(array)//2 - 1, -1, -1): 
         maxHeapify(array,  i) 
     return array
 
+# Makes last child as parent and calls heapify to top which fixes up tree down the path
 def deleteMax(array) -> bool:
     if len(array) < 1:
         return False
     array[0] = array.pop()
     maxHeapify(array,0)
+    return True
+
+# Delete first element and re-structure heap. Keep doing this until only 1 element.
+def heapSort(array):
+    array = buildHeap(array)
+    
+    sortedList = []
+    for _ in range(len(array),1,-1):
+        sortedList.append(array[0])
+        deleteMax(array)
+    sortedList.append(array[0])
+    return sortedList
 
 array = [12, 11, 13, 5, 6, 7]
 heap = buildHeap(array)
-print(heap)
-deleteMax(array)
-print(heap)
+print(heapSort(array))
